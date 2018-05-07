4349d9777a6e66022129cde00e20147251a7dc69
corgan <corganhejijun@gmail.com>
2018-05-07 09:56:06 +0800

    change out batch size the same with input batch size

--- model.py
+++ model.py
@@ -26,8 +26,8 @@ def dataset_files(root):
 
 class DCGAN(object):
     def __init__(self, sess, image_size=128, is_crop=False,
-                 batch_size=64, sample_size=128, lowres=8,
-                 z_dim=100, gf_dim=128, df_dim=128,
+                 batch_size=64, sample_size=64, lowres=8,
+                 z_dim=100, gf_dim=64, df_dim=64,
                  gfc_dim=1024, dfc_dim=1024, c_dim=3,
                  checkpoint_dir=None, lam=0.1):
         """
